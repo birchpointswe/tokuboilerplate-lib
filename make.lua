9a342692ec849fed49f0dda97baee209d819a4ef
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-28 13:50:14 -0400

    bump santoku to 2.0.0, < 3.0.0, santoku-fs to 2.0.0, < 3.0.0 +2

--- make.lua
+++ make.lua
@@ -2,24 +2,29 @@ local env = {
   name = "tokuboilerplate",
   version = "0.0.1-1",
   license = "MIT",
+  public = false,
   cflags = {
     "-Wall", "-Wextra",
     "-I$(shell luarocks show santoku --rock-dir)/include/",
   },
   dependencies = {
     "lua == 5.1",
-    "santoku >= 1.0.0, < 2.0.0",
-    "santoku-sqlite >= 1.0.0, < 2.0.0",
-    "santoku-sqlite-migrate >= 1.0.0, < 2.0.0",
+    "santoku >= 2.0.0, < 3.0.0",
+    "santoku-sqlite >= 2.0.0, < 3.0.0",
+    "santoku-sqlite-migrate >= 2.0.0, < 3.0.0",
     "lsqlite3 >= 0.9.6-1",
     "argparse >= 0.7.1-1",
   },
   test = {
     dependencies = {
-      "santoku-fs >= 1.0.0, < 2.0.0",
+      "santoku-fs >= 2.0.0, < 3.0.0",
     },
   },
 }
+
+env.homepage = "https://github.com/YOUR-GITHUB-USER/" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
   type = "lib",
